52c2c3f1a640e841cb2f77d52f0379de48d31047
agiss615 <sooin615@naver.com>
2022-11-23 10:29:56 +0900

    🎉 :: 카페 관련한 앱입니다.

---
+++ CafeApp/CafeApp/CafeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CafeAppApp.swift
+//  CafeApp
+//
+//  Created by Agiss on 2022/11/10.
+//
+
+import SwiftUI
+
+@main
+struct CafeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
